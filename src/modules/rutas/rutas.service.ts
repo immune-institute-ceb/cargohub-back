709f352219aaad3007077a473e244a0fdadac98f
w0lfpy <jose.suarez.ceb@immune.institute>
2025-05-25 13:06:53 +0200

    [feature]: New controllers, modules and services for routes endpoints

---
+++ src/modules/rutas/rutas.service.ts
@@ -0,0 +1,145 @@
+// Objective: Implement the service to manage the user entity.
+
+//* NestJS modules
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+//* External modules
+import { Model } from 'mongoose';
+import { instanceToPlain } from 'class-transformer';
+
+//* DTOs
+import { RegisterRutaDto, UpdateRutaDto } from '@modules/rutas/dto';
+
+//* Services
+import { ExceptionsService } from '@common/exceptions/exceptions.service';
+
+//* Entities
+import { Ruta } from './entities/rutas.entity';
+
+@Injectable()
+export class RutasService {
+  constructor(
+    @InjectModel(Ruta.name)
+    private readonly rutaModel: Model<Ruta>,
+    private readonly exceptionsService: ExceptionsService,
+  ) {}
+
+  async create(registerRutaDto: RegisterRutaDto) {
+    try {
+      const rutaCreated = await this.rutaModel.create(registerRutaDto);
+      return rutaCreated;
+    } catch (error) {
+      this.exceptionsService.handleDBExceptions(error);
+    }
+  }
+
+  async update(ruta: Ruta, updateRutaDto: UpdateRutaDto) {
+    try {
+      const { ...update } = instanceToPlain(updateRutaDto);
+
+      const rutaUpdated = await this.rutaModel.findOneAndUpdate(
+        { _id: ruta._id },
+        update,
+        {
+          new: true,
+        },
+      );
+
+      if (!rutaUpdated) throw new NotFoundException('Ruta not found');
+
+      return {
+        message: 'Ruta updated',
+        rutaUpdated,
+      };
+    } catch (error) {
+      this.exceptionsService.handleDBExceptions(error);
+    }
+  }
+
+  async deleteRuta(ruta: Ruta) {
+    try {
+      await this.rutaModel.findOneAndDelete({ _id: ruta._id });
+      return { message: 'Ruta deleted' };
+    } catch (error) {
+      this.exceptionsService.handleDBExceptions(error);
+    }
+  }
+
+  async archiveRuta(ruta: Ruta) {
+    try {
+      const rutaToArchive = await this.rutaModel.findById(ruta._id);
+
+      if (!rutaToArchive) throw new NotFoundException('Ruta not found');
+
+      const rutaArchived = await this.rutaModel.findOneAndUpdate(
+        { _id: ruta._id },
+        { isDeleted: true, deletedAt: new Date() },
+        { new: true },
+      );
+
+      if (!rutaArchived) throw new NotFoundException('Ruta not found');
+
+      return { message: 'Ruta archived' };
+    } catch (error) {
+      this.exceptionsService.handleDBExceptions(error);
+    }
+  }
+
+  async restoreRuta(ruta: Ruta) {
+    try {
+      const rutaToRestore = await this.rutaModel.findById(ruta._id);
+
+      if (rutaToRestore?.isDeleted === false)
+        throw new BadRequestException('Ruta not archived');
+
+      const rutaRestored = await this.rutaModel.findOneAndUpdate(
+        { _id: ruta._id },
+        { isDeleted: false, deletedAt: null },
+        { new: true },
+      );
+
+      if (!rutaRestored) throw new NotFoundException('Ruta not found');
+
+      return { message: 'Ruta restored' };
+    } catch (error) {
+      this.exceptionsService.handleDBExceptions(error);
+    }
+  }
+
+  async findAllRutas() {
+    try {
+      const rutas = await this.rutaModel.find();
+      if (!rutas) throw new NotFoundException('Rutas not found');
+      return rutas;
+    } catch (error) {
+      this.exceptionsService.handleDBExceptions(error);
+    }
+  }
+
+  async findRutaById(id: string) {
+    try {
+      const ruta = await this.rutaModel.findById(id);
+
+      if (!ruta) throw new NotFoundException('Ruta not found');
+
+      return ruta;
+    } catch (error) {
+      this.exceptionsService.handleDBExceptions(error);
+    }
+  }
+
+  async findRutasByType(type: string) {
+    try {
+      const rutas = await this.rutaModel.find({ type });
+      if (!rutas) throw new NotFoundException('Rutas not found');
+      return rutas;
+    } catch (error) {
+      this.exceptionsService.handleDBExceptions(error);
+    }
+  }
+}
